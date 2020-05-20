2645b85bebb1fb179d42e0ac3651beee71f5c62b
Aaron Bruce <aaronbruce555@gmail.com>
2020-05-20 16:13:31 -0400

    Complete version 1

--- mole.js
+++ mole.js
@@ -1,7 +1,13 @@
 function popUpRandomMole() {
     const moleHead = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
-    const randNum = Math.floor(Math.random() * (7 - 0 + 1)) + 0;
-    const visible = moleHead[randNum] === undefined ? popUpRandomMole() : moleHead[randNum];
+    if (moleHead.length === 0){
+        alert("No more moles to who-wack!");
+        return;
+    }
+    const randNum = Math.floor(Math.random() * ((moleHead.length - 1) - 0) + 0);
+
+    const visible = moleHead[randNum];
+    // === undefined ? popUpRandomMole() : moleHead[randNum]; // All hail the cheese
     visible.classList.remove('wgs__mole-head--hidden');
     setTimeout(() => hideMole(visible), 1000);
 }
@@ -14,9 +20,7 @@ function hideMole(mole) {
 window.addEventListener('DOMContentLoaded', () => {
     let count = 0;
     popUpRandomMole();
-    // hideMole(visible);
 
-    // make a click event for the moleHead
     let shownMole = document.querySelectorAll(".wgs__mole-head")
     for (const mole of shownMole) {
         mole.addEventListener('click', event => {
